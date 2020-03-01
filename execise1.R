13d9c3836828bfce21b6f424fa72a515bbeb9755
AntiD2ta <mtpotro41@gmail.com>
2020-03-01 12:55:13 -0500

    refs #1 - Implement pipeline for exercise 1
    Changes:
    - Add bar, pie and line charts for analysis of students grades
    
    Co-authored-by: stdevRulo <riglesiasvera@gmail.com>

--- execise1.R
+++ execise1.R
@@ -8,3 +8,79 @@ process_data <- function(data) {
   r <- table(cut(data, breaks=c(-1, 9, 11, 13, 15, 20), labels=c("[0,10)", "[10,12)", "[12,14)", "[14,16)", "[16,20]")))
   return(r)
 }
+
+percent <- function(x, s) {
+  return(round((x * 100 / s)))
+}
+
+bar_data <- function(x) {
+  lbls <- c("Fail", "Sufficient", "Satisfactory", "Good", "Excellent/Very Good")
+  cls <- c("red", "light green", "dark green", "blue", "dark blue")
+  barplot(x, ylim=c(0,130), col=cls, legend=lbls)
+}
+
+pie_data <- function(x) {
+  s <- sum(x)
+  pct <- round(percent(x, s))
+  lbls <- c("Fail", "Sufficient", "Satisfactory", "Good", "Excellent/Very Good")
+  lbls2 <- paste(lbls, " ", pct, "%", sep="")
+  cls <- c("red", "light green", "dark green", "blue", "dark blue")
+  pie(x, labels=lbls2, col=cls)
+}
+
+map <- function(l, f, args) {
+  temp <- c()
+  for (i in 1:length(l)) {
+    temp[i] <- f(l[i], args)
+  }
+  return(temp)
+}
+
+pipeline <- function(data) {
+  d1 <- process_data(data$G1.y)
+  d2 <- process_data(data$G2.y)
+  d3 <- process_data(data$G3.y)
+  
+  bar_data(d1)
+  bar_data(d2)
+  bar_data(d3)
+  
+  pie_data(d1)
+  pie_data(d2)
+  pie_data(d3)
+}
+
+
+to_line_chart <- function(f1, f2, f3) {
+  df <- data.frame(f1, f2, f3)
+  y <- c()
+  x <- c()
+  for (i in 1:length(f1)) {
+    y <- c(y, 1, 2, 3)
+    x <- c(x, df$Freq[i], df$Freq.1[i], df$Freq.2[i])
+  }
+  plot(y, x, type="n", ylab = "frequence", xlab="time")
+  cls <- c("red", "light green", "dark green", "blue", "dark blue")
+  linetype <- c(1:length(f1))
+  plotchar <- seq(18, 18+length(f1), 1)
+  for (i in 0:(length(f1)-1)) {
+    lines(y[(i*3+1):(i*3+3)], x[(i*3+1):(i*3+3)], 
+          type="b", 
+          lwd=2, 
+          lty=linetype[i + 1], 
+          col=cls[i + 1], 
+          pch=plotchar[i + 1] 
+    ) 
+  }
+}
+
+exercise3 <- function(data) {
+  t <- table(d$G3.y, d$sex)
+  M <- t[1:(length(t)/2), 1]
+  F <- t[1:(length(t)/2), 2]
+  return(c(mean(M), mean(F)))
+}
+
+for (i in 1:5) {
+  x <- c(x, d1[i], d2[i], d3[i, 2])
+}
